771370eaf9159a15254ca2a4810e886233dd167a
YoungProducer <sashabezrukovownmail@gmail.com>
2020-03-21 14:06:05 +0200

    Added selectors for 'testSuite' slice and tests for this selectors.

---
+++ src/store/selectors/__tests__/testSuite.test.ts
@@ -0,0 +1,334 @@
+/**
+ * Created by: Oleksandr Bezrukov
+ * Creation date 21 March 2020
+ *
+ * Test suite for 'testSuite' selectors.
+ */
+
+/** Application's imports */
+import { RootState } from "store/slices";
+import {
+    selectTestSuiteLoading,
+    selectTestSuiteLimitTime,
+    selectShowRightDuringTest,
+    selectTestSuitesTasksImages,
+    selectTestSuiteExplanationsImages,
+    selectSelectedAnswers,
+    selectGivedAnswers,
+    selectRightAnswers,
+    selectSelectedAnswerByTaskIndex,
+    selectGivedAnswerByTaskIndex,
+    selectIsAnswerSelected,
+    selectIsAnswerGived,
+    selectIsAnswerRight,
+} from "../testSuite";
+
+describe('TestSuite selectors', () => {
+    test('selectTestSuiteLoading', () => {
+        /** Define state */
+        const state = {
+            testSuite: {
+                loading: false,
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectTestSuiteLoading(state);
+
+        /** Assert result of selector has right value */
+        expect(result).toBeFalsy();
+    });
+
+    test('selectTestSuiteLimitTime', () => {
+        /** Define state */
+        const state = {
+            testSuite: {
+                limitTime: false,
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectTestSuiteLimitTime(state);
+
+        /** Assert result of selector has right value */
+        expect(result).toBeFalsy();
+    });
+
+    test('selectShowRightDuringTest', () => {
+        /** Define mocked state */
+        const state  = {
+            testSuite: {
+                showRightDuringTest: false,
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectShowRightDuringTest(state);
+
+        /** Assert result of selector has right value */
+        expect(result).toBeFalsy();
+    });
+
+    test('selectTestSuiteTasksImages', () => {
+        /** Define mocked state */
+        const state  = {
+            testSuite: {
+                tasksImages: ['foo'],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectTestSuitesTasksImages(state);
+
+        /** Assert result of selector has right keys */
+        expect(result).toEqual(['foo']);
+    });
+
+    test('selectTestSuiteExplanationsImages', () => {
+        /** Define mocked state */
+        const state  = {
+            testSuite: {
+                explanationImages: ['foo'],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectTestSuiteExplanationsImages(state);
+
+        /** Assert result of selector has right keys */
+        expect(result).toEqual(['foo']);
+    });
+
+    test('selectSelectedAnswers', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                selectedAnswers: [['0']],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectSelectedAnswers(state);
+
+        /** Assert result of selector has right keys */
+        expect(result).toEqual([['0']]);
+    });
+
+    test('selectGivedAnswers', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                givedAnswers: [['2', '1']],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectGivedAnswers(state);
+
+        /** Assert result of selector has right keys */
+        expect(result).toEqual([['2', '1']]);
+    });
+
+    test('selectRightAnswers', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                rightAnswers: [['2', '1']],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectRightAnswers(state);
+
+        /** Assert result of selector has right keys */
+        expect(result).toEqual([['2', '1']]);
+    });
+
+    test('selectSelectedAnswerByTaskIndex', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                selectedAnswers: [
+                    ['2', '1'],
+                    ['3', '4'],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectSelectedAnswerByTaskIndex(state, { taskIndex: 1 });
+
+        /** Assert selector returns right value */
+        expect(result).toEqual(['3', '4']);
+    });
+
+    test('selectGivedAnswerByTaskIndex', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                givedAnswers: [
+                    ['2', '1'],
+                    ['3', '4'],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectGivedAnswerByTaskIndex(state, { taskIndex: 1 });
+
+        /** Assert selector returns right value */
+        expect(result).toEqual(['3', '4']);
+    });
+
+    test('selectIsAnswerSelected with already selected answers should return true', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                selectedAnswers: [
+                    ['2', '1'],
+                    ['3', '4'],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectIsAnswerSelected(state, { taskIndex: 1 });
+
+        /** Assert selector returns right value */
+        expect(result).toBeTruthy();
+    });
+
+    test('selectIsAnswerSelected without selected answers should return false', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                selectedAnswers: [
+                    ['2', '1'],
+                    ['', ''],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectIsAnswerSelected(state, { taskIndex: 1 });
+
+        /** Assert selector returns right value */
+        expect(result).toBeFalsy();
+    });
+
+    test('selectIsAnswerSelected when only one answer is selected should return true', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                selectedAnswers: [
+                    ['2', '1'],
+                    ['3', ''],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectIsAnswerSelected(state, { taskIndex: 1 });
+
+        /** Assert selector returns right value */
+        expect(result).toBeTruthy();
+    });
+
+    test('selectIsAnswerGived when only one answer is selected should return false', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                givedAnswers: [
+                    ['2', '1'],
+                    ['3', ''],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectIsAnswerGived(state, { taskIndex: 1 });
+
+        /** Assert selector returns right value */
+        expect(result).toBeFalsy();
+    });
+
+    test('selectIsAnswerGived if all answers are not selected should return false', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                givedAnswers: [
+                    ['2', '1'],
+                    ['', ''],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectIsAnswerGived(state, { taskIndex: 1 });
+
+        /** Assert selector returns right value */
+        expect(result).toBeFalsy();
+    });
+
+    test('selectIsAnswerGived if all answers are selected should return true', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                givedAnswers: [
+                    ['2', '1'],
+                    ['2', '1'],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectIsAnswerGived(state, { taskIndex: 1 });
+
+        /** Assert selector returns right value */
+        expect(result).toBeTruthy();
+    });
+
+    test('selectIsAnswerRight if all answers are right should return true', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                givedAnswers: [
+                    ['2', '1'],
+                    ['2', '1'],
+                ],
+                rightAnswers: [
+                    ['2', '1'],
+                    ['3', '4'],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectIsAnswerRight(state, { taskIndex: 0 });
+
+        /** Assert selector returns right value */
+        expect(result).toBeTruthy();
+    });
+
+    test('selectIsAnswerRight if one answer are incorrect should return false', () => {
+        /** Define mocked state */
+        const state = {
+            testSuite: {
+                givedAnswers: [
+                    ['2', '1'],
+                    ['2', '1'],
+                ],
+                rightAnswers: [
+                    ['2', '1'],
+                    ['2', '4'],
+                ],
+            },
+        } as RootState;
+
+        /** Get selector's result */
+        const result = selectIsAnswerRight(state, { taskIndex: 1 });
+
+        /** Assert selector returns right value */
+        expect(result).toBeFalsy();
+    });
+});
---
+++ src/store/selectors/testSuite.ts
@@ -0,0 +1,75 @@
+/**
+ * Created by: Oleksandr Bezrukov
+ * Creation date: 21 March 2020
+ *
+ * Selectors for 'testSuite' slice.
+ */
+
+/** External imports */
+import { createSelector } from '@reduxjs/toolkit';
+import { ParametricSelector } from 'reselect';
+
+/** Application's imports */
+import { RootState } from 'store/slices';
+
+const selectTaskIndexFromProps: ParametricSelector<RootState, any, number> = (_, props) => props.taskIndex;
+
+export const selectTestSuiteLoading = (state: RootState) =>
+    state.testSuite.loading;
+
+export const selectTestSuiteLimitTime = (state: RootState) =>
+    state.testSuite.limitTime;
+
+export const selectShowRightDuringTest = (state: RootState) =>
+    state.testSuite.showRightDuringTest;
+
+export const selectTestSuitesTasksImages = (state: RootState) =>
+    state.testSuite.tasksImages;
+
+export const selectTestSuiteExplanationsImages = (state: RootState) =>
+    state.testSuite.explanationImages;
+
+export const selectSelectedAnswers = (state: RootState) =>
+    state.testSuite.selectedAnswers;
+
+export const selectGivedAnswers = (state: RootState) =>
+    state.testSuite.givedAnswers;
+
+export const selectRightAnswers = (state: RootState) =>
+    state.testSuite.rightAnswers;
+
+export const selectIsAnswerSelected = createSelector(
+    selectSelectedAnswers,
+    selectTaskIndexFromProps,
+    (selectedAnswers, taskIndex) =>
+        selectedAnswers[taskIndex].some(answer => answer !== ''),
+);
+
+export const selectIsAnswerGived = createSelector(
+    selectGivedAnswers,
+    selectTaskIndexFromProps,
+    (givedAnswers, taskIndex) =>
+        givedAnswers[taskIndex].every(answer => answer !== ''),
+);
+
+export const selectIsAnswerRight = createSelector(
+    selectGivedAnswers,
+    selectRightAnswers,
+    selectTaskIndexFromProps,
+    (givedAnswers, rightAnswers, taskIndex) =>
+        givedAnswers[taskIndex].every((answer, index) =>
+            answer === rightAnswers[taskIndex][index]),
+);
+
+export const selectSelectedAnswerByTaskIndex = createSelector(
+    selectSelectedAnswers,
+    selectTaskIndexFromProps,
+    (selectedAnswers, taskIndex) =>
+        selectedAnswers[taskIndex]);
+
+export const selectGivedAnswerByTaskIndex = createSelector(
+    selectGivedAnswers,
+    selectTaskIndexFromProps,
+    (givedAnswers, taskIndex) =>
+        givedAnswers[taskIndex],
+);
